097e0244f0be684b4c13511cf6c782f68bc19d67
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2017-10-02 13:41:19 +0200

    add temporary dev database setup

--- apps/sgc/config/test.exs
+++ apps/sgc/config/test.exs
@@ -3,8 +3,9 @@ use Mix.Config
 # Configure your database
 config :sgc, SGC.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
   database: "sgc_test",
-  hostname: "localhost",
+  hostname: System.get_env("POSTGRES_HOSTNAME") || "localhost",
+  port: System.get_env("POSTGRES_PORT") || 5432,
   pool: Ecto.Adapters.SQL.Sandbox
