dac9e3a7c19eb708646356f8778130094791d239
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2017-10-24 16:58:22 +0200

    add UpdateCurrentUser plug to refresh the user data every 10 minutes

---
+++ apps/sgc_web/lib/sgc_web/plugs/update_current_user.ex
@@ -0,0 +1,43 @@
+defmodule SGCWeb.Plugs.UpdateCurrentUser do
+  @moduledoc """
+  A `Plug` to update the current user data
+  """
+
+  import Plug.Conn
+  import SGCWeb.Session, only: [current_user: 1]
+  import SGCWeb.Controllers.Helpers
+  alias SGC.StargateCommand
+
+  def init(options), do: options
+
+  def call(conn, _opts) do
+    if user = current_user(conn) do
+      conn |> update_user_data(user)
+    else
+      conn
+    end
+  end
+
+  def update_user_data(conn, %{user_id: id, updated_at: updated}) do
+    diff = DateTime.diff(DateTime.utc_now(), updated)
+    cond do
+      diff > 600 -> conn |> fetch_data_and_assign(id)
+      true -> conn
+    end
+  end
+  def update_user_data(conn, %{user_id: id}) do
+    conn |> fetch_data_and_assign(id)
+  end
+  def update_user_data(conn, _user), do: conn
+
+  def fetch_data_and_assign(conn, id) do
+    cookies = create_cookie_list(conn)
+    {:ok, user_data, _} = StargateCommand.user_info(id, cookies)
+
+    user_data = Map.put(user_data, :updated_at, DateTime.utc_now())
+
+    conn
+    |> put_session(:current_user, user_data)
+    |> assign(:current_user, user_data)
+  end
+end
